f071dd89072933f9516aaed192d430659a6e0df1
Fertaz <96764860+Fertaz@users.noreply.github.com>
2025-08-16 14:45:26 +0600

    Add files via upload

--- ShilpokalaAC/src/main/java/com/example/shilpokalaac/U7EventCoordinatorDashboard.java
+++ ShilpokalaAC/src/main/java/com/example/shilpokalaac/U7EventCoordinatorDashboard.java
@@ -1,7 +1,13 @@
 package com.example.project_by_pushon;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class U7EventCoordinatorDashboard
 {
@@ -13,34 +19,82 @@ public class U7EventCoordinatorDashboard
     }
 
     @javafx.fxml.FXML
-    public void SetupChecklistONA(ActionEvent actionEvent) {
+    public void SetupChecklistONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u7_g4_Event_checklist.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void VendorRequestsONA(ActionEvent actionEvent) {
+    public void VendorRequestsONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u7_g3_Vendor_request.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void SecurityCoordinationONA(ActionEvent actionEvent) {
+    public void SecurityCoordinationONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u7_g7_Security_Notes.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void ResourceAllocationONA(ActionEvent actionEvent) {
+    public void ResourceAllocationONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u7_g2_Resource_management.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void IncidentReportsONA(ActionEvent actionEvent) {
+    public void IncidentReportsONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("U7IncidentReports.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void OnGroundStatusONA(ActionEvent actionEvent) {
+    public void OnGroundStatusONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u7_g5_ground_event_status.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void ArchiveReportsONA(ActionEvent actionEvent) {
+    public void ArchiveReportsONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u7_g8_Archive_Reports.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void ScheduleRehearsalsONA(ActionEvent actionEvent) {
+    public void ScheduleRehearsalsONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u7_g1_Schedule_Rehearsal.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 }
--- ShilpokalaAC/src/main/java/com/example/shilpokalaac/U8FinanceOfficerDashboard.java
+++ ShilpokalaAC/src/main/java/com/example/shilpokalaac/U8FinanceOfficerDashboard.java
@@ -1,7 +1,13 @@
 package com.example.project_by_pushon;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class U8FinanceOfficerDashboard
 {
@@ -13,19 +19,43 @@ public class U8FinanceOfficerDashboard
     }
 
     @javafx.fxml.FXML
-    public void MonthlyReportsONA(ActionEvent actionEvent) {
+    public void MonthlyReportsONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u8_g2_Financial_Reports.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void ProcessArtistPaymentsONA(ActionEvent actionEvent) {
+    public void ProcessArtistPaymentsONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u8_g1_Process_Payments.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void TicketRevenueONA(ActionEvent actionEvent) {
+    public void TicketRevenueONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u8_g5_Ticket_Revenue.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void ApproveExpensesONA(ActionEvent actionEvent) {
+    public void ApproveExpensesONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u8_g3_Approve_Expenses.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
@@ -37,10 +67,22 @@ public class U8FinanceOfficerDashboard
     }
 
     @javafx.fxml.FXML
-    public void VendorInvoicesONA(ActionEvent actionEvent) {
+    public void VendorInvoicesONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u8_g4_Manage_Invoices.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void SalaryDisbursementONA(ActionEvent actionEvent) {
+    public void SalaryDisbursementONA(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("u8_g6_Salary_Disbursement.fxml"));
+        Parent parent = loader.load();
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene newScene = new Scene(parent);
+        currentStage.setScene(newScene);
+        currentStage.show();
     }
 }
